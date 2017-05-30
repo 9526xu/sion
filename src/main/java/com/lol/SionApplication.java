4ada5fd79b25071a55223989d4c06257f718781f
xiaohei <xiaohei@maihaoche.com>
2017-05-30 13:28:38 +0800

    thymeleaf 初始化

--- src/main/java/com/lol/SionApplication.java
+++ src/main/java/com/lol/SionApplication.java
@@ -3,7 +3,7 @@ package com.lol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.lol")
 public class SionApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/lol/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.lol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author andyXu(xiaohei) xiaohei@maihaoche.com
+ * @Date 2017/5/30
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("host", "www.baidu.com");
+
+        return "index";
+    }
+}
