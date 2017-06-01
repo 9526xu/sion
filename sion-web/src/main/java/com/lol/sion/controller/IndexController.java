c9190b7416268dc3c1c7bf9c0c12bc370a63ca75
xiaohei <xiaohei@maihaoche.com>
2017-06-01 17:13:47 +0800

    工程搭建初始化

--- sion-web/src/main/java/com/lol/sion/controller/IndexController.java
+++ sion-web/src/main/java/com/lol/sion/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.lol.sion.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @Author andyXu(xiaohei) xiaohei@maihaoche.com
  * @Date 2017/5/30
  */
+@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping("/index")
     public String index(Model model) {
+        log.info("log4j2 日志测试");
         model.addAttribute("host", "www.baidu.com");
 
         return "index";
