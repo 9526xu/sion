deadca30a1a4fdd824571d669d1fed425e97b274
andy.xu <xiaohei@maihaoche.com>
2017-08-15 23:51:51 +0800

    增加 metrics 点击次数统计

--- sion-web/src/main/java/com/lol/sion/controller/IndexController.java
+++ sion-web/src/main/java/com/lol/sion/controller/IndexController.java
@@ -8,6 +8,9 @@ import com.lol.sion.core.service.intf.NasdaqEarningService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.CollectionUtils;
@@ -33,14 +36,22 @@ public class IndexController {
     @Autowired
     private NasdaqEarningService nasdaqEarningService;
 
-    @RequestMapping(value = "/index",method = RequestMethod.GET)
+
+    @Autowired
+    CounterService counterService;
+
+
+    @Autowired
+    GaugeService gaugeService;
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String index(Model model) {
         log.info("log4j2 日志测试");
         model.addAttribute("host", "www.baidu.com");
         return "index";
     }
 
-    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
         log.info("查找 list 开始");
         NasdqEarningQuery query = new NasdqEarningQuery();
@@ -49,25 +60,27 @@ public class IndexController {
         model.addAttribute("list", listResponses);
         model.addAttribute("time", DateFormatUtils.format(new Date(), "yyyy-MM-dd"));
 
-        Map<String,Integer> timeMap= Maps.newHashMap();
+        Map<String, Integer> timeMap = Maps.newHashMap();
 
         //计算数量
-        if(!CollectionUtils.isEmpty(listResponses)){
+        if (!CollectionUtils.isEmpty(listResponses)) {
             listResponses.forEach(nasdaqEarningListResponse -> {
-                String time=nasdaqEarningListResponse.getPublishTime();
-                if(time.equals("盘前")){
-                    time="before";
-                }else if(time.equals("盘后")){
-                    time="after";
-                }else {
-                    time="unknown";
+                String time = nasdaqEarningListResponse.getPublishTime();
+                if (time.equals("盘前")) {
+                    time = "before";
+                } else if (time.equals("盘后")) {
+                    time = "after";
+                } else {
+                    time = "unknown";
                 }
 
-                timeMap.merge(time,1,(oldNum, newNum) -> oldNum+newNum);
+                timeMap.merge(time, 1, (oldNum, newNum) -> oldNum + newNum);
             });
         }
-        model.addAttribute("timeMap",timeMap);
+        model.addAttribute("timeMap", timeMap);
 
+        counterService.increment("nasdaq.list");
+        gaugeService.submit("nasdaq.list.last.visit",System.currentTimeMillis());
         return "index";
     }
 
